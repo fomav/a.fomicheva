48dfc3308e92b246e190d524c81a14291b98c9e8
Sasha <a.fomicheva@2gis.ru>
2016-05-09 21:25:40 +0600

    4 занятие

---
+++ FourthLesson/Lesson/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    class Telephone
+    {
+        private string cityCode;
+        private string numberPhone;
+
+        //Конструктор.
+        public  Telephone(string cCode, string phNumber)
+        {
+            cityCode = cCode;
+            numberPhone = phNumber;
+        }
+
+        public string CityCode
+        {
+            get
+            {
+                return cityCode;
+            }
+            set
+            {
+                cityCode = value;
+            }
+        }
+
+        public string NumberPhone
+        {
+            get
+            {
+                return numberPhone;
+            }
+            set
+            {
+                numberPhone = value;
+            }
+        }
+        public string transformNumber()
+        {
+            if ((cityCode == null) || (cityCode==""))
+            {
+                return numberPhone;
+            }
+            else return "(" + cityCode + ")" + numberPhone;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telephone MyPhone = new Telephone("383","2665013");
+            Console.WriteLine(MyPhone.transformNumber());
+            Console.ReadLine();
+            Console.WriteLine(MyPhone.CityCode);
+            Console.WriteLine(MyPhone.NumberPhone);
+            Console.ReadLine();
+        }
+    }
+}
