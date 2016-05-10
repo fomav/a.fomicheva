918982391ef9e7133b9b5d06fd41b13a17a2e8fc
Sasha <a.fomicheva@2gis.ru>
2016-05-10 16:32:54 +0600

    1

---
+++ FifthLesson/Lesson/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    class client
+    {
+        protected int id;
+        protected int phoneNumber;
+        protected float orderPrice;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+        public float OrderPrice
+        {
+            get { return orderPrice; }
+            set { orderPrice = value; }
+        }
+
+        public client (float orderPrice) 
+            {
+            Console.WriteLine(String.Format("Сумма заказа {0:C2}.", orderPrice));
+        }
+
+
+    }
+}
---
+++ FifthLesson/Lesson/ClientOOO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    class clientOOO : client
+    {
+        private string name;
+        private string bankAccount;
+        
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string BankAccount
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public clientOOO(string name,float orderPrice) 
+            {
+            Console.WriteLine(String.Format("ООО {0}:", name));
+            client.cl
+
+        }
+         
+
+    }
+}
